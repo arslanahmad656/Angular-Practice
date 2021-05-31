d99f6a6685245e52efd142cce3eda8bc57268615
Arslan Ahmad <arslan.ahmad@imanami.com>
2021-05-31 23:42:54 +0500

    Added model

--- forms-app/src/app/app.module.ts
+++ forms-app/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,7 +16,8 @@ import { ReactiveFormComponent } from './components/reactive-form/reactive-form.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- forms-app/src/app/components/reactive-form/reactive-form.component.ts
+++ forms-app/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.scss']
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
+  registrationModel: FormGroup;
+  
+  constructor() { 
+    this.registrationModel = new FormGroup({
+      username: new FormControl('Usman'),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
+      address: new FormGroup({
+        street: new FormControl(''),
+        city: new FormControl('Shakargarh'),
+        state: new FormControl(''),
+        country: new FormControl('')
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
