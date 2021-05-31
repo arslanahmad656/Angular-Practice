def5f99c55c073133dbacb59feb73e0f926626f4
Arslan Ahmad <arslan.ahmad@imanami.com>
2021-06-01 02:16:22 +0500

    Simple validations added

--- forms-app/src/app/components/reactive-form/reactive-form.component.ts
+++ forms-app/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { formGroupRequiredValidator } from 'src/app/validators/form-group-required.validator';
+import { sameNameValidator } from 'src/app/validators/same-name.validator';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,6 +10,38 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
   registrationModel: FormGroup;
+
+  get username() {
+    return this.registrationModel.get('username');
+  }
+
+  get password() {
+    return this.registrationModel.get('password');
+  }
+
+  get confirmPassword() {
+    return this.registrationModel.get('confirmPassword');
+  }
+
+  get street() {
+    return this.registrationModel.get('address')?.get('street');
+  }
+
+  get city() {
+    return this.registrationModel.get('address')?.get('city');
+  }
+
+  get state() {
+    return this.registrationModel.get('address')?.get('state');
+  }
+
+  get country() {
+    return this.registrationModel.get('address')?.get('country');
+  }
+
+  get address() {
+    return this.registrationModel.get('address');
+  }
   
   constructor(
     private readonly formBuilder: FormBuilder
@@ -15,7 +49,7 @@ export class ReactiveFormComponent implements OnInit {
     let group = this.formBuilder.group.bind(this.formBuilder);
     let control = this.formBuilder.control.bind(this.formBuilder);
     this.registrationModel = group({
-      username: control('Usman'),
+      username: control('', Validators.required),
       password: control(''),
       confirmPassword: control(''),
       address: group({
@@ -23,8 +57,8 @@ export class ReactiveFormComponent implements OnInit {
         city: control('Shakargarh'),
         state: control(''),
         country: control('')
-      })
-    });
+      }, {validator: formGroupRequiredValidator(['street', 'city', 'country'])})
+    }, {validator: sameNameValidator(['password', 'confirmPassword'])});  
   }
 
   ngOnInit(): void {
